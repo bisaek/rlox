085f4a1a1ddb65533734a01f9e09e95022d49065
bisaek <huggerbertram@gmail.com>
2026-02-22 13:13:26 +0100

    handle keywords

--- src/scanner.rs
+++ src/scanner.rs
@@ -101,7 +101,28 @@ impl Scanner {
         while self.peek().is_alphanumeric() {
             self.advance();
         }
-        self.add_token(TokenType::Identifier);
+
+        let text = &self.source[self.start..self.current];
+        let token_type = match text {
+            "and" => TokenType::And,
+            "class" => TokenType::Class,
+            "else" => TokenType::Else,
+            "false" => TokenType::False,
+            "for" => TokenType::For,
+            "fun" => TokenType::Fun,
+            "if" => TokenType::If,
+            "nil" => TokenType::Nil,
+            "or" => TokenType::Or,
+            "print" => TokenType::Print,
+            "return" => TokenType::Return,
+            "super" => TokenType::Super,
+            "this" => TokenType::This,
+            "true" => TokenType::True,
+            "var" => TokenType::Var,
+            "while" => TokenType::While,
+            _ => TokenType::Identifier,
+        };
+        self.add_token(token_type);
     }
 
     fn number(&mut self) {
